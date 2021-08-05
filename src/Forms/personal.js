1d78fe9c0d4cc540d7a59dd2f5471c6a355133fd
Luis Tesorero <luistesorero5@gmail.com>
2021-08-05 17:04:45 -0500

    fix

--- src/Forms/personal.js
+++ src/Forms/personal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Countrys from '../countrys.json';
 import { Validation } from '../Helpers';
+import dates from '../date.json';
 function DatosPersonales(props) {
     const { setData, dato } = props;
     return (
@@ -36,10 +37,8 @@ function DatosPersonales(props) {
                         <div className="grid gap-2 grid-cols-3">
                             <div className="inline-block relative">
                                 <select onChange={(e) => setData({ ...dato, DD: e.target.value })} className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                                    <option value="01">01</option>
-                                    <option value="01">01</option>
-                                    <option value="01">01</option>
-                                    <option value="01">01</option>
+                                    <option disabled selected>DD</option>
+                                    {dates.dd.map((item, index) => <option key={index} value={item}>{item}</option>)}
                                 </select>
                                 <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                     <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
@@ -47,10 +46,8 @@ function DatosPersonales(props) {
                             </div>
                             <div className="inline-block relative">
                                 <select onChange={(e) => setData({ ...dato, MM: e.target.value })} className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                                    <option value="01">01</option>
-                                    <option value="01">01</option>
-                                    <option value="01">01</option>
-                                    <option value="01">01</option>
+                                    <option disabled selected>MM</option>
+                                    {dates.mm.map((item, index) => <option key={index} value={item}>{item}</option>)}
                                 </select>
                                 <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                     <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
@@ -58,10 +55,8 @@ function DatosPersonales(props) {
                             </div>
                             <div className="inline-block relative">
                                 <select onChange={(e) => setData({ ...dato, YY: e.target.value })} className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                                    <option value="01">01</option>
-                                    <option value="01">01</option>
-                                    <option value="01">01</option>
-                                    <option value="01">01</option>
+                                    <option disabled selected>YYYY</option>
+                                    {dates.yy.map((item, index) => <option key={index} value={item}>{item}</option>)}
                                 </select>
                                 <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                     <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
@@ -79,6 +74,7 @@ function DatosPersonales(props) {
                         </label>
                         <div className="inline-block relative w-full">
                             <select onChange={(e) => setData({ ...dato, pais: e.target.value })} className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                                <option disabled selected>Selecciona país</option>
                                 {Countrys.map((item, index) => <option key={index} value={item.code}>{item.name}</option>)}
                             </select>
                             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
